7f2eecdafdd530a7965c9acfc0c464ba60ede0a9
Patricia <chorriballong@gmail.com>
2014-10-06 14:26:06 +0200

    changed artifactId & names (failed dependency, maybe unnessesary)

--- src/main/java/cd/ForumResource.java
+++ src/main/java/cd/ForumResource.java
@@ -16,6 +16,119 @@ import javax.ws.rs.core.Response;
  */
 @Path("products") // Leading trailing slash doesn't matter, see web.xml
 public class ForumResource {
+      
+//    private final IForum forum = SingletonForum.INSTANCE.getForum();
+    
+//    FROM WS2
+//    private final IShop shop = SingletonShop.INSTANCE.getShop();
+//    private final IProductCatalogue products = shop.getProductCatalogue();
+//    
+//    @POST
+//    @Consumes(value = MediaType.APPLICATION_JSON)
+//    @Produces(value = {MediaType.APPLICATION_JSON})
+//    public Response create(JsonObject json) {
+//        
+//        String name = json.getString("name");
+//        double price = (double) json.getInt("price");
+//         
+//        try{
+//            Product p = new Product(name, price);    
+//            products.create(p);
+//            ProductWrapper pw = new ProductWrapper(p);
+//            
+//            return Response.ok(pw).build();
+//        } catch (IllegalArgumentException e) {
+//            return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+//        }
+//    }
+//
+//    @DELETE
+//    @Path(value = "{id}")
+//    public Response delete(@PathParam(value = "id") final Long id) {
+//        
+//        try {
+//            products.delete(id);
+//            return Response.ok().build();
+//        } catch (IllegalArgumentException e) {
+//            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+//        }
+//    }
+//
+//    @PUT
+//    @Path(value = "{id}")
+//    @Consumes(value = MediaType.APPLICATION_JSON)
+//    @Produces(value = {MediaType.APPLICATION_JSON})
+//    public Response update(@PathParam(value = "id") final Long id, JsonObject json) {
+//        
+//        String name = json.getString("name");
+//        double price = (double) json.getInt("price");
+//        Product p = new Product(id, name, price);
+//        products.update(p);
+//        
+//        ProductWrapper pw = new ProductWrapper(p);
+//        return Response.ok(pw).build();
+//        
+//    }
+//
+//    @GET
+//    @Produces(value = {MediaType.APPLICATION_JSON})
+//    @Path(value = "{id}")
+//    public Response find(@PathParam(value = "id") Long id) {
+//        
+//        Product p = products.find(id);
+//        ProductWrapper pw = new ProductWrapper(p); 
+//        if (pw != null) {
+//            return Response.ok(pw).build();
+//        } else {
+//            return Response.noContent().build();
+//        }
+//    }
+//
+//    @GET
+//    @Produces(value = {MediaType.APPLICATION_JSON})
+//    public Response findAll() {
+//   
+//        List<Product> old = products.findAll();
+//        
+//        ProductWrapper pw;
+//        List<ProductWrapper> newList = new ArrayList<>();
+//        for(Product p: old){
+//            pw = new ProductWrapper(p);
+//            newList.add(pw);
+//            
+//        }
+//        
+//        GenericEntity<List<ProductWrapper>> geAll = new GenericEntity<List<ProductWrapper>>(newList){};
+//        return Response.ok(geAll).build();
+//    }
+//
+//    @GET
+//    @Path(value = "range")
+//    @Produces(value = {MediaType.APPLICATION_JSON})
+//    public Response findRange(@QueryParam(value = "first") int first, @QueryParam(value = "n") int n) {
+//       
+//        List<Product> range = products.findRange(first, n);
+//        
+//        ProductWrapper pw;
+//        List<ProductWrapper> newList = new ArrayList<>();
+//        for(Product p: range){
+//            pw = new ProductWrapper(p);
+//            newList.add(pw);
+//        }
+//        
+//        GenericEntity<List<ProductWrapper>> geRange = new GenericEntity<List<ProductWrapper>>(newList) {};
+//        return Response.ok(geRange).build();
+//    }
+//
+//    @GET
+//    @Path(value = "count")
+//    @Produces(value = {MediaType.APPLICATION_JSON})
+//    public Response count() {
+//        
+//        int c = shop.getProductCatalogue().count();
+//        JsonObject value = Json.createObjectBuilder().add("value", c).build();
+//        return Response.ok(value).build();
+//    }
     
     //METHODS
     //Examples from WS2
---
+++ src/main/java/core/SingletonForum.java
@@ -0,0 +1,22 @@
+package core;
+
+/**
+ 
+ * A wrapper for the shop. We should only have
+ * one shop (better use CDI more later)
+ * @author 
+ */
+ 
+
+//Possibly use and rework
+public enum SingletonForum {
+
+//    INSTANCE;
+//    
+//    private final IForum forum = forum.newInstance();
+//
+//    public IForum getForum() {
+//        return forum;
+//    }
+
+}
