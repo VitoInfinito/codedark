7b5f06778e62f2249f53e89b7709a67c9b4c3e10
Hampus Forsvall <forzvall@gmail.com>
2014-10-21 12:54:46 +0200

    Coursescraper
    Scrapes courses from chalmers website

---
+++ src/main/webapp/forum/app/js/courseScraper.js
@@ -0,0 +1,31 @@
+function scrape(){
+	function Course() {
+		courseCode: ;
+		courseName: ;
+	}
+
+	var fetchedCourses = [];
+
+	$.ajax({
+		url: 'https://www.student.chalmers.se/sp/course_list?flag=1&query_start=1&batch_size=1310&sortorder=CODE&search_ac_year=2014/2015',
+		type: 'get',
+		dataType: 'html',
+		success: function(data) {
+			data = data.replace(/<img\b[^>]*>/ig, '');
+			data = data.replace(/<script.*?>([\w\W\d\D\s\S\0\n\f\r\t\v\b\B]*?)<\/script>/gi, '');
+			var tBody = $(data).find('table')[2].tBodies[0];
+			var secondTbl = tBody.rows[0].cells[0];
+			console.log(secondTbl+ "Hej");
+			console.log($(data).find('table').children('tbody')[12]);		
+		}
+	});
+
+}
+
+
+scrape();
+
+
+
+
+
