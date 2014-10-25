8fbb0dfc08ce59b3039d1436a648cfcc3f0172c1
Hampus Forsvall <forzvall@gmail.com>
2014-10-25 18:32:57 +0200

    Tests

--- src/test/java/cds/core/TestPersistence.java
+++ src/test/java/cds/core/TestPersistence.java
@@ -6,6 +6,7 @@
 package cds.core;
 
 
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.enterprise.inject.Default;
@@ -20,6 +21,8 @@ import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
@@ -39,6 +42,8 @@ public class TestPersistence {
     UserTransaction utx;
     
     private final static Logger log = Logger.getAnonymousLogger();
+    //True enables all instances of log
+    private final static boolean D = true;
     
     @PersistenceContext(unitName = "jpa_forum_test_pu")
     @Produces
@@ -73,21 +78,20 @@ public class TestPersistence {
         //Will always fail to check if the testing is working.
         //assertTrue(1 == 2);
     }
-    
+    // --- GROUPTESTS --- //
     @Test
-    public void testPersistAUser() {
-        GroupUser u = new GroupUser("a" ,"aaa@a.a", "pwd", "fnmae", "lname");
-        forum.getUserList().create(u);
-        GroupUser u1 = forum.getUserList().find(u.getId());
-        assertTrue(u.equals(u1));
-    }
-    
-    @Test
-    public void testPersistACourse(){
-        Course c = new Course("1234", "testCourse");
+    public void testGroupGetByNameAndCourse(){
+        Course c = new Course("DAT998", "Testkursen2");
         forum.getCourseList().create(c);
-        Course c2 = forum.getCourseList().getById(c.getId());
-        assertTrue(c2.equals(c));                
+        GroupUser user = new GroupUser("t", "a@a.se", "1", "test", "er");
+        forum.getUserList().create(user);
+        CourseGroup u = new CourseGroup(c, "Testgruppen", user, 2);
+        if(D) log.log(Level.WARNING, u+"");
+        forum.getGroupList().create(u);
+        
+        CourseGroup u1 = forum.getGroupList().getByNameAndCourse(u.getgName(), c.getId());
+        if(D) log.log(Level.INFO, u + " ||| " + u1);
+        assertTrue(u.equals(u1));
     }
     
     @Test
@@ -101,24 +105,79 @@ public class TestPersistence {
         CourseGroup g1 = forum.getGroupList().find(g.getId());
         assertTrue(g.equals(g1));
     }
-
-    public void testUserDelete(){
-//        GroupUser u = new GroupUser(1234L, "aaa@a.a", "pwd", "a", "b", false);
-//        forum.getUserList().create(u);
-//        assertTrue(forum.getUserList().count() == 1);
-//        forum.getUserList().delete(u.getId());
-//        assertTrue(forum.getUserList().count() == 0);
-    }
     
     @Test
     public void testGroupDelete(){
-//        Course c = new Course("DAT999", "Testkursen");
-//        forum.getCourseList().create(c);
-//        CourseGroup u = new CourseGroup(c, "Testgrupp", null, 2);
-//        forum.getGroupList().create(u);
-//        assertTrue(forum.getGroupList().count() == 1);
-//        forum.getGroupList().delete(u.getId());
-//        assertTrue(forum.getGroupList().count() == 0);
+        Course c = new Course("DAT999", "Testkursen");
+        forum.getCourseList().create(c);
+        GroupUser u = new GroupUser("tester", "aaa@a.a", "pwd", "a", "b");
+        forum.getUserList().create(u);
+        CourseGroup g = new CourseGroup(c, "Testgrupp", u, 2);
+        forum.getGroupList().create(g);
+        assertNotNull(forum.getGroupList().find(g.getId()));
+        forum.getGroupList().delete(g.getId());
+        assertNull(forum.getGroupList().find(g.getId()));
+    }
+    
+    @Test
+    public void testGroupGetByCourse(){
+        String cId = "DAT999";
+        Course c = new Course(cId, "Testkursen");
+        forum.getCourseList().create(c);
+        GroupUser u = new GroupUser("tester", "aaa@a.a", "pwd", "a", "b");
+        forum.getUserList().create(u);
+        CourseGroup g = new CourseGroup(c, "Testgrupp", u, 2);
+        forum.getGroupList().create(g);
+        int index = forum.getGroupList().getByCourse(cId).indexOf(g);
+        CourseGroup g2 = forum.getGroupList().getByCourse(cId).get(index);
+        assertTrue(g.equals(g2));
+    }
+    @Test
+    public void testGroupGetByUser(){
+        String uId = "tester";
+        Course c = new Course("DAT999", "Testkursen");
+        forum.getCourseList().create(c);
+        GroupUser u = new GroupUser(uId, "aaa@a.a", "pwd", "a", "b");
+        forum.getUserList().create(u);
+        CourseGroup g = new CourseGroup(c, "Testgrupp", u, 2);
+        forum.getGroupList().create(g);
+        int index = forum.getGroupList().getByUser(uId).indexOf(g);
+        CourseGroup g2 = forum.getGroupList().getByUser(uId).get(index);
+        assertTrue(g.equals(g2));
+    }
+    // --- USERTESTS --- //
+    @Test
+    public void testUserDelete(){
+        GroupUser u = new GroupUser("tester", "aaa@a.a", "pwd", "a", "b");
+        forum.getUserList().create(u);
+        assertTrue(forum.getUserList().find(u.getId()).equals(u));
+        forum.getUserList().delete(u.getId());
+        assertNull(forum.getUserList().find(u.getId()));
+    }
+    
+    @Test
+    public void testPersistAUser() {
+        GroupUser u = new GroupUser("a" ,"aaa@a.a", "pwd", "fnmae", "lname");
+        forum.getUserList().create(u);
+        GroupUser u1 = forum.getUserList().find(u.getId());
+        assertTrue(u.equals(u1));
+    }
+    
+    @Test
+    public void testUserGetByID() {
+        String username = "tester";
+        GroupUser u = new GroupUser(username, "aaa@a.a", "pwd", "a", "b");
+        forum.getUserList().create(u);
+        GroupUser u2 = forum.getUserList().getById(username);
+        assertTrue(u2.getId() == u.getId());
+    }
+    // --- COURSETESTS --- //
+    @Test
+    public void testPersistACourse(){
+        Course c = new Course("1234", "testCourse");
+        forum.getCourseList().create(c);
+        Course c2 = forum.getCourseList().getById(c.getId());
+        assertTrue(c2.equals(c));                
     }
     
     @Test
@@ -126,26 +185,12 @@ public class TestPersistence {
         Course c = new Course("TST999", "Testkurs");
         forum.getCourseList().create(c);
         assertTrue(forum.getCourseList().count() == 1);
+        if(D) log.log(Level.INFO, "Number of courses: " + forum.getCourseList().count());
         System.out.println(c.getId());
         forum.getCourseList().delete(c.getId());
         assertTrue(forum.getCourseList().count() == 0);
+        if(D) log.log(Level.INFO, "Number of courses: " + forum.getCourseList().count());
     }
-    
-    @Test
-    public void testGroupGetByName(){
-        Course c = new Course("DAT998", "Testkursen2");
-        forum.getCourseList().create(c);
-        GroupUser user = new GroupUser("t", "a@a.se", "1", "test", "er");
-        forum.getUserList().create(user);
-        CourseGroup u = new CourseGroup(c, "Testgruppen", user, 2);
-        log.log(Level.WARNING, u+"");
-        forum.getGroupList().create(u);
-        
-        CourseGroup u1 = forum.getGroupList().getByNameAndCourse(u.getgName(), c.getId());
-        log.log(Level.INFO, u + " ||| " + u1);
-        assertTrue(u.equals(u1));
-    }
-    
     @Test
     public void testCoursegetById(){
         Course c = new Course("DAT997", "Testkursen");
@@ -154,14 +199,13 @@ public class TestPersistence {
         Course c1 = forum.getCourseList().getById("DAT997");
         assertTrue(c.equals(c1));
     }
-    
     @Test
-    public void testUserGetBySsnbr() {
-//        Long ssnbr = 1234L;
-//        GroupUser u = new GroupUser(ssnbr, "aaa@a.a", "pwd", "a", "b", false);
-//        forum.getUserList().create(u);
-//        GroupUser u2 = forum.getUserList().getBySsnbr(ssnbr);
-//        assertTrue(u2.getSsnbr() == u.getSsnbr());
+    public void testCourseGetByName(){
+        String cName = "Testing";
+        Course c = new Course("AAA000",cName);
+        forum.getCourseList().create(c);
+        int index = forum.getCourseList().getByName(cName).indexOf(c);
+        assertNotNull(forum.getCourseList().getByName(cName).get(index));
     }
 }
 
