9ca95939936d7276cafadfbd0e96a9b067b36187
terrorulf <sofia.edstrom@galaxen.se>
2014-10-26 19:10:41 +0100

    ..

--- src/main/java/cds/core/Course.java
+++ src/main/java/cds/core/Course.java
@@ -17,34 +17,7 @@ import cds.persistence.AbstractEntity;
 @Entity
 public class Course extends AbstractEntity<String>{
     
-    private String name;
     
-    public Course(){
-        
-    }
-    
-    public Course(String ccode, String name){
-        super(ccode);
-        this.name = name;
-    }
-    
-    public Course(String name){
-        this.name = name;
-    }
-    
-    public String getName(){
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    
-    @Override
-    public String toString(){
-        return "Course{ ccode = " + getId() + ", name = " + name + "}";
-    }
     
     @Override
     public boolean equals(Object obj) {
